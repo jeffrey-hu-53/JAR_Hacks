14bcba03eeb6e45c046f3e903cf62ff2686843b3
jeffrey-hu-53 <jeffrey.hu@stonybrook.edu>
2022-02-12 13:00:15 -0500

    Main Screen 1.1 -donator and donee button are linked to a new screen -(NO BACK BUTTON YET)

---
+++ app/src/main/java/com/example/jar_hacks/DonatorActivity.java
@@ -0,0 +1,14 @@
+package com.example.jar_hacks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.donator_main);
+    }
+}
