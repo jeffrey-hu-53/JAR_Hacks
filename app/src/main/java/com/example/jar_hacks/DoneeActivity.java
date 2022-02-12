c3434e84dbbf11631b4f55c83709e4ce58833321
jeffrey-hu-53 <jeffrey.hu@stonybrook.edu>
2022-02-12 14:28:29 -0500

    Donee Screen -Added a map button, when clicked it opens google maps and takes in stony brooks coordinates

--- app/src/main/java/com/example/jar_hacks/DoneeActivity.java
+++ app/src/main/java/com/example/jar_hacks/DoneeActivity.java
@@ -2,7 +2,12 @@ package com.example.jar_hacks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class DoneeActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class DoneeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.donee_main);
+
+        Button doneeMapButt = (Button) findViewById(R.id.doneeMapButt);
+
+        //Coordinates so google maps knows where to zoom in
+        float lat = (float)40.9047962;
+        float lon = (float)-73.1327196;
+
+        doneeMapButt.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Uri gmmIntentUri = Uri.parse("geo:" + lat + "," + lon + "?q=");
+                        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                        mapIntent.setPackage("com.google.android.apps.maps");
+                        startActivity(mapIntent);
+                    }
+                }, 1000);
+            }
+        });
     }
 }
+
+
