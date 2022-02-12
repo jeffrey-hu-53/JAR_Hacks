3e8658ba72a319aa4967686b82e5237d3b6eb3c8
Alan Yi <Alan.yi@stonybrook.edu>
2022-02-12 17:57:44 -0500

    gradle database

--- app/src/main/java/com/example/jar_hacks/MainActivity.java
+++ app/src/main/java/com/example/jar_hacks/MainActivity.java
@@ -7,12 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        mDatabase = FirebaseDatabase.getInstance().getReference().child("JAR");
+//        mDatabase.push().setValue("hello");
+
 
         Button donatorButt = (Button) findViewById(R.id.donatorButt);
 
